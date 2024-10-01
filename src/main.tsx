311a819e3551f89565af906fb48b245fb47216bb
bskimball <brian.kimball@bdkonline.com>
2024-10-01 11:23:52 -0400

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <App />
+  },
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+)
